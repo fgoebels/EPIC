6f066f73ceff127a71d23a92a2ecf5bb147fa16f
LucasHuUofT <lucasminghu@Lucass-MacBook-Pro.local>
2016-09-05 20:07:34 -0400

    update script

--- src/ExtractGeneMANIA.py
+++ src/ExtractGeneMANIA.py
@@ -10,8 +10,10 @@ proteinsPairDictionary = {}
 
 with open(sys.argv[1]) as myfile:    
     for line in myfile:
-        proteinA, proteinB= line.split()
-        edge = "\t".join(sorted([proteinA, proteinB]))
+        #proteinA, proteinB= line.split()
+        items = line.split()
+        #edge = "\t".join(sorted([proteinA, proteinB]))
+        edge = "\t".join(sorted([items[0], items[1]]))
         proteinsPairDictionary[edge] = ""
 
 # @author: Lucas Ming Hu
@@ -24,15 +26,26 @@ def fillZero(proteinsPairDictionary, proteinsInFile):
     for protein in proteins_wih_no_evidence:
         proteinsPairDictionary[protein] += "\t0"
 
-#all functional evidence codes in GeneMANIA
-functionalEvidences = ['Co-expression', 'Genetic', 'other', 'Physical', 'Predicted', 'Shared']
+#all functional evidence codes in GeneMANIA, excluding "Physical" and "complexes" to eliminate circularity
+functionalEvidences = ['Co-expression', 'Genetic', 'other', 'Predicted', 'Shared']
+excludingFunctionalEvidences = ['Physical', 'complexes']
 
 #read files of GeneMANIA database and add features to 
 for filename in glob.glob('*.txt'):
     has_evidence = False
     for f_eveidence in functionalEvidences:
         if f_eveidence in filename:
+            
             has_evidence = True
+            
+            for e_evidence in excludingFunctionalEvidences:
+                
+                if e_evidence in filename:
+            
+                    has_evidence = False
+            
+            
+            
     if not has_evidence: continue
     print "Processing: %s" % (filename)
     fh = open(filename)
