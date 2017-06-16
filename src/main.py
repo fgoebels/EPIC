684838e0ea63ce127bb25b8585eb62b7ceffd2f4
LucasHu <ming.sebastian.unsw@gmail.com>
2017-06-16 17:04:06 -0400

    change a few things

--- src/main.py
+++ src/main.py
@@ -60,7 +60,8 @@ def main():
 
 
 	# Predict protein interaction
-	network =  utils.make_predictions(scoreCalc, mode, clf, train, functionalData)
+	#network =  utils.make_predictions(scoreCalc, mode, clf, train, functionalData)
+	network = utils.make_predictions(scoreCalc, mode, clf, all_gs, functionalData)
 	outFH = open("%s.%s.pred.txt" % (output_dir, mode + anno_source), "w")
 	print >> outFH, "\n".join(network)
 	outFH.close()
